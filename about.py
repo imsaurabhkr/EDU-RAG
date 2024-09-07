cea9809c03b173d6a2bacf0dc10fc86cd736e450
ak1484 <ankitsaharan1484@gmail.com>
2024-09-07 19:48:45 +0530

    first commit

---
+++ about.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+def app():
+    st.subheader('Pondering is a website created for users to')
+    st.subheader('share their valuable thoughts with the world.')
+    st.markdown('Created by: [Ankit](https://github.com/ak1484)')
+    st.markdown('Contact via mail: [ankitsaharan1484@gmail.com]')
+    
---
+++ main.py
@@ -0,0 +1,71 @@
+import streamlit as st
+
+from streamlit_option_menu import option_menu
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+import home, trending, account, your, about, buy_me_a_coffee
+st.set_page_config(
+        page_title="Class",
+)
+
+
+st.markdown(
+    """
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src=f"https://www.googletagmanager.com/gtag/js?id={os.getenv('analytics_tag')}"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', os.getenv('analytics_tag'));
+        </script>
+    """, unsafe_allow_html=True)
+print(os.getenv('analytics_tag'))
+
+class MultiApp:
+
+    def __init__(self):
+        self.apps = []
+
+    def add_app(self, title, func):
+        self.apps.append({
+            "title": title,
+            "function": func
+        })
+
+    def run(self):  # Add 'self' as a parameter here
+        with st.sidebar:
+            app = option_menu(
+                menu_title='Class',
+                options=['Home', 'Account', 'Trending', 'Your Posts', 'about', 'Buy_me_a_coffee'],
+                icons=['house-fill', 'person-circle', 'trophy-fill', 'chat-fill', 'info-circle-fill'],
+                menu_icon='chat-text-fill',
+                default_index=1,
+                styles={
+                    "container": {"padding": "5!important", "background-color": 'black'},
+                    "icon": {"color": "white", "font-size": "23px"},
+                    "nav-link": {"color": "white", "font-size": "20px", "text-align": "left", "margin": "0px", "--hover-color": "blue"},
+                    "nav-link-selected": {"background-color": "#02ab21"}
+                }
+            )
+
+        # Call the respective app based on the sidebar selection
+        if app == "Home":
+            home.app()
+        elif app == "Account":
+            account.app()
+        elif app == "Trending":
+            trending.app()
+        elif app == 'Your Posts':
+            your.app()
+        elif app == 'about':
+            about.app()
+        elif app == 'Buy_me_a_coffee':
+            buy_me_a_coffee.app()
+
+
+# Instantiate the class and run the app
+multi_app = MultiApp()
+multi_app.run()
