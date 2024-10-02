051d5800b263b095caf83c6fb0e4f57a4a586ade
imsaurabhkr <saurabh_k@ma.iitr.ac.in>
2024-10-02 23:02:21 +0530

    commit1

--- about.py
+++ about.py
@@ -3,6 +3,6 @@ import streamlit as st
 def app():
     st.subheader('Pondering is a website created for users to')
     st.subheader('share their valuable thoughts with the world.')
-    st.markdown('Created by: [Ankit](https://github.com/ak1484)')
-    st.markdown('Contact via mail: [ankitsaharan1484@gmail.com]')
+    st.markdown('Created by: [Saurabh kumar](https://github.com/imsaurabhkr)')
+    st.markdown('Contact via mail: [saurabhkumar841301@gmail.com]')
     
